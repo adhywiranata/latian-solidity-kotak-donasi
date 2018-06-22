a65962d6497f7fafdadcd8fd4e32bc440fabd8e2
Adhy <adhywiranatap@gmail.com>
2018-06-22 07:39:20 +0700

    update compile script and view

--- compile.js
+++ compile.js
@@ -5,4 +5,5 @@ const solc = require('solc');
 const contractPath = path.resolve(__dirname, '', 'my-donation-box.sol');
 const source = fs.readFileSync(contractPath, 'utf8');
 const compiled = solc.compile(source, 1);
+console.log(compiled.contracts[':MyDonationBox'].interface);
 module.exports = compiled.contracts[':MyDonationBox'];
